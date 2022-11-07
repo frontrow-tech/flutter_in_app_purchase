454c22565bbbf3395402bd1e1c0758b2251214ca
Gangadhara Shetty P J <gangadharashetty.gs@gmail.com>
2022-11-07 17:17:16 +0530

    reverted null saftey back to null

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_inapp_purchase/FlutterInappPurchasePlugin.h>)
+#import <flutter_inapp_purchase/FlutterInappPurchasePlugin.h>
+#else
+@import flutter_inapp_purchase;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterInappPurchasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterInappPurchasePlugin"]];
+}
+
+@end
